e375d349699b0eda99fe9990996b13b4b81e13f8
WilliamSCH <williamg.schreiner@gmail.com>
2023-04-14 20:15:46 -0300

    create card e componentes

--- PROG_WEBIII/projectFront01/src/App.jsx
+++ PROG_WEBIII/projectFront01/src/App.jsx
@@ -7,18 +7,28 @@ export function App() {
 
   return (
    <div className={style.container}>
-    <div className={style.box}>  
-      <div>
-      <img  className={style.cover} src="https://images.unsplash.com/photo-1536227661368-deef57acf708?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"></img>
-      <h3 >Amilto Smaniotto</h3>
-      <h4 >Desempregado</h4>
+      <div className={style.card}>
+        <img
+          src="https://images.unsplash.com/photo-1536227661368-deef57acf708?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
+          alt="Cover"
+        />
+        <img
+          className={style.avatar}
+          src="https://github.com/WilliamSchreiner.png"
+          alt="Avatar"
+        />
+        <p className={style.p1}>William Schreiner</p>
+        <p className={style.p2}>Jovem Aprendiz desenvovedor</p>
+        <div className={style.stack}>
+          <span>#Humano</span>
+          <span>#AtéAMorte</span>
+          <span>#Sextou!</span>
+          <span>#Anão</span>
+          <span>#Dando</span>
+          <span>#Pirueta</span>
+        </div>
+      </div>
     </div>
-    <div>
-      <h5 >#Cria</h5>
-      <h5  >#Cavalo</h5>
-    </div>
-  </div>  
-</div>
   )
 }
 
