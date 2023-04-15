9daf1b284bf7fbc211b8e5d652ed6b188b2a4ca4
WilliamSCH <williamg.schreiner@gmail.com>
2023-04-14 21:10:41 -0300

    componentizacoa

--- PROG_WEBIII/projectFront01/src/App.jsx
+++ PROG_WEBIII/projectFront01/src/App.jsx
@@ -8,29 +8,7 @@ export function App() {
 
   return (
    <div className={style.container}>
-      <div className={style.card}>    
-        <img
-          src="https://images.unsplash.com/photo-1536227661368-deef57acf708?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
-          alt="Cover"
-        />
-        <img
-          className={style.avatar}
-          src="https://github.com/WilliamSchreiner.png"
-          alt="Avatar"
-        />
-        <p className={style.p1}>William Schreiner</p>
-        <p className={style.p2}>Jovem Aprendiz desenvovedor</p>
-        <CardPerson> </CardPerson>
-        <div className={style.stack}>
-          <span>#Humano</span>
-          <span>#AtéAMorte</span>
-          <span>#Sextou!</span>
-          <span>#Anão</span>
-          <span>#Dando</span>
-          <span>#Pirueta</span>
-        </div>
-      </div>
-      
+    <CardPerson></CardPerson>
     </div>
   )
 }
