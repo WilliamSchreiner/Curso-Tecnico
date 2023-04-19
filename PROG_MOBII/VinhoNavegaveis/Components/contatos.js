a3fbde0534ce9cb15e79f2616bdc02d0b1d405a3
WilliamSCH <williamg.schreiner@gmail.com>
2023-04-18 22:22:51 -0300

    create app de catalogo de vinhos, aula 18/04

---
+++ PROG_MOBII/VinhoNavegaveis/Components/contatos.js
@@ -0,0 +1,19 @@
+import { Text, View } from 'react-native';
+import stylesBody from './style';
+
+export default function Contatos() {
+  return (
+    <View style={stylesBody.container}>
+      <Text>Entre em contato conosco. Para encomendar produtos.</Text>      
+      <View>
+
+      </View>
+      <View>
+        
+      </View>
+      <View>
+        
+      </View>
+    </View>
+  );
+}
