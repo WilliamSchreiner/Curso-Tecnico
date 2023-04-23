a982c55acdc0a2b544b1f44aad2dd0eafb9be467
WilliamSchreiner <williamg.schreiner@gmail.com>
2023-04-23 00:48:26 -0300

    style global estilizando a aba catalogo e contatos

--- PROG_MOBII/VinhoNavegaveis/Components/style.js
+++ PROG_MOBII/VinhoNavegaveis/Components/style.js
@@ -16,7 +16,75 @@ const stylesBody = StyleSheet.create({
         fontSize: 18,
         color: '#fff'
     },
+    catalogo:{
+    backgroundColor: '#fff',
+    height:'100%',
+    width: '100%'
+    },
+    titleCatalogo:{
+        fontSize: 24,
+        marginTop: '10%',
+        marginBottom: '10%',
+        marginLeft: '10%',
+        fontWeight: 'bold'
+    },
+    textCatalogo:{
+        fontSize: 16,
+        marginBottom: '10%',
+        marginLeft: '10%'
+    },
+    vinhoLista:{
+        alignContent: 'center',
+        alignItems: 'center'
+    },
+    boxVinho:{
+        border: 'solid',
+        borderWidth: 1,
+        borderRadius: 15,
+        height:'40%',
+        width: '90%',
+        marginBottom: '5%',
+        alignItems: 'center'
+    },
+    vinhoImg:{
+        height: '80%',
+        width: '30%'
+    },
+    vinhoBoxTextos:{},
+    titleBoxVinho:{},
+    textBoxVinho:{},
 
+    contato:{
+    backgroundColor: '#fff',
+    height:'100%',
+    width: '100%'
+    },
+    titleContato:{
+        fontSize: 22,
+        marginTop: '10%',
+        marginBottom: '10%',
+        marginLeft: '10%',
+        fontWeight: 'bold'
+    },
+    contatoLista:{
+        alignContent: 'center',
+        alignItems: 'center'
+    },
+    contatoBox:{
+        border: 'solid',
+        borderWidth: 1,
+        borderRadius: 15,
+        height:'30%',
+        width: '80%',
+        marginBottom: '5%',
+        alignItems: 'center'
+    },
+    titleBoxContato:{
+        fontSize: 16,
+        fontWeight: 'bold',
+        color: '#400303'
+    },
+    textBoxContato:{}
 
 
   });
