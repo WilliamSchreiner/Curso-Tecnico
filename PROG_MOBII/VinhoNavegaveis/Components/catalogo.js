f9a83a7ff14385c13ed6fe6ecb98e7dd9048d051
WilliamSchreiner <williamg.schreiner@gmail.com>
2023-04-23 00:49:13 -0300

    create aba catalogo e contatos

--- PROG_MOBII/VinhoNavegaveis/Components/catalogo.js
+++ PROG_MOBII/VinhoNavegaveis/Components/catalogo.js
@@ -1,12 +1,31 @@
-import { Text, View } from 'react-native';
+import { Text, View, Image} from 'react-native';
 import stylesBody from './style';
 
 export default function Catalogo() {
   return (
-    <View style={stylesBody.container}>
-      <Text>Nossos Vinhos</Text>
-      <Text>Trabalhamos com o melhor vinho dos seguintes tipos: Vinho Branco, Vinho rosé, Vinho tinto e Vinho seco.</Text>    
+    <View style={stylesBody.catalogo}>
+      <Text style={stylesBody.titleCatalogo}>Nossos Vinhos</Text>
+      <Text style={stylesBody.textCatalogo}>Trabalhamos com o melhor vinho dos seguintes tipos: Vinho Branco, Vinho rosé, Vinho tinto e Vinho seco.</Text>    
       
+      <View style={ stylesBody.vinhoLista }>
+        <View style={ stylesBody.boxVinho }>
+          <Image resizeMode="contain" style={ stylesBody.vinhoImg } source={require('../assets/vinho-branco.jpg')} />
+          <View style={ stylesBody.vinhoBoxTextos}>
+            <Text style={ stylesBody.titleBoxVinho}>Chatigny Chardonnay</Text>
+            <Text style={ stylesBody.textBoxVinho}>Vinho leve, refrescante e levemente cítrico da cor amarelo palha. Perfeito com carnes brancas e massa ao pesto.</Text>
+          </View>
+        </View>
+        <View style={ stylesBody.boxVinho }>
+          <Image resizeMode="contain" style={stylesBody.vinhoImg } source={require('../assets/vinho-rose.jpg')} />
+          <View style={ stylesBody.vinhoBoxTextos}>
+            <Text style={ stylesBody.titleBoxVinho}>Vinho Branco</Text>
+            <Text style={ stylesBody.textBoxVinho}>Vinho leve, refrescante e levemente cítrico da cor amarelo palha. Perfeito com carnes brancas e massa ao pesto.</Text>
+          </View>
+        </View>
+
+      </View>
+         
+
     </View>
   );
 }
--- PROG_MOBII/VinhoNavegaveis/Components/contatos.js
+++ PROG_MOBII/VinhoNavegaveis/Components/contatos.js
@@ -1,19 +1,37 @@
 import { Text, View } from 'react-native';
 import stylesBody from './style';
+import { FontAwesome, Entypo, MaterialIcons } from '@expo/vector-icons';
 
 export default function Contatos() {
   return (
-    <View style={stylesBody.container}>
-      <Text>Entre em contato conosco. Para encomendar produtos.</Text>      
-      <View>
+   <View style={ stylesBody.contato }>
+        <Text style={ stylesBody.titleContato }>Entre em contato conosco para comprar nossos produtos</Text>
+      
+        <View style={ stylesBody.contatoLista }>
+          <View style={ stylesBody.contatoBox }>
+            <FontAwesome name="phone" size={ 50 } color="#400303" />
+            <Text style={ stylesBody.titleBoxContato}>Telefone:</Text>
+            <Text style={ stylesBody.textBoxContato}>+55 21 000000000</Text>
+          </View>
+          
+          <View style={ stylesBody.contatoBox }>
+            <Entypo name="location-pin" size={ 50 } color="#400303" />
+            <Text style={ stylesBody.titleBoxContato}>Endereço:</Text>
+            <Text style={ stylesBody.textBoxContato}>Av. 123, 222 - Rio de Janeiro RJ</Text>
+          </View>
 
+          <View style={ stylesBody.contatoBox }>
+            <MaterialIcons name="email" size={ 50 } color="#400303" />
+            <Text style={ stylesBody.titleBoxContato}>Email:</Text>
+            <Text style={ stylesBody.textBoxContato}>preferida@adega.com.br</Text>
+          </View>
+
+          <View style={stylesBody.contatoBox }>
+            <FontAwesome name="instagram" size={ 50 } color="#400303" />
+            <Text style={stylesBody.titleBoxContato}>Instagram:</Text>
+            <Text style={ stylesBody.textBoxContato}>@adegapreferida</Text>
+          </View>
+        </View>
       </View>
-      <View>
-        
-      </View>
-      <View>
-        
-      </View>
-    </View>
   );
 }
