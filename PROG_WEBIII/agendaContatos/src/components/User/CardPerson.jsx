7a12b407bdd9a48ecf2228260a2901487ed9ca88
WilliamSchreiner <williamg.schreiner@gmail.com>
2023-05-10 17:00:58 -0300

    create e estilização dos span

--- PROG_WEBIII/agendaContatos/src/components/User/CardPerson.jsx
+++ PROG_WEBIII/agendaContatos/src/components/User/CardPerson.jsx
@@ -9,6 +9,11 @@ export function CardPerson({ cover, avatar, name, office, stacks }) {
       <img className={stylePerson.avatar} src={avatar} alt="Avatar" />
       <p className={stylePerson.p1}>{name}</p>
       <p className={stylePerson.p2}>{office}</p>
+
+      <div className={stylePerson.spans}>
+      <span >#Brazilian</span>
+      <span >#JunDev</span>
+      </div>
       
     </div>
   );
