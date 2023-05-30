e61b7bf192d1fee071ba886a41b2ca63089e9887
William <williag.schreiner@gmail.com>
2023-05-29 21:43:33 -0300

    inicial function Pesquisar

--- PROG_WEBIII/agendaContatos/Front/src/components/Home/Home.jsx
+++ PROG_WEBIII/agendaContatos/Front/src/components/Home/Home.jsx
@@ -4,33 +4,31 @@ import style from "./Home.module.css";
 import { CardPerson } from "../User/CardPerson";
 import { CardContato, Delete, Update } from "../Contato/CardContatos";
 //imports Icons
-import {BsFillTrash3Fill } from "react-icons/bs";
-import {IoPencil, IoSearchOutline, IoAdd } from "react-icons/io5";
+import { BsFillTrash3Fill } from "react-icons/bs";
+import { IoPencil, IoSearchOutline, IoAdd } from "react-icons/io5";
 
 import { useEffect, useState } from "react";
 import { api, server } from "../../api/axios";
 
 
 export function Home() {
-  
-   let [persons, setPersons] = useState([]);
- 
-   useEffect(() => {
+
+  let [persons, setPersons] = useState([]);
+  let [nome, setNome] = useState('');
+
+  useEffect(() => {
     Load()
-}, []);
+  }, []);
 
   async function Load() {
     const temp = await server.get("user/");
-    console.log(temp)
+      console.log(temp)
 
     setPersons(temp.data);
-    
-    console.log(persons);
   }
 
-  async function Adicionar(){
+  async function Adicionar() {
     event.preventDefault();
-    console.log("função insert");
 
     const resultAPI = await api.get("/");
 
@@ -39,94 +37,109 @@ export function Home() {
     const nomeCompleto = `${user.name.first} ${user.name.last}`;
     const avatar = user.picture.large;
     const cell = user.cell;
-    
+
     await server.post("user/", {
-    name: nomeCompleto,
-    avatar: avatar,
-    celular: cell,
+      name: nomeCompleto,
+      avatar: avatar,
+      celular: cell,
     });
 
     Load();
   }
 
   async function Deletar() {
-  event.preventDefault();
+    event.preventDefault();
 
-  Delete();
-  Load();
+    Delete();
+    Load();
+  }
+
+  function Pesquisar() {
+    event.preventDefault();
+    console.log(`nome procurado ${nome}`);
+    Load();
+
+    if (nome === '') { console.log("Escreva algum nome.") }
+    const results = []
+    persons.map((user => {
+      console.log(user.name)
+      if(user.name.toLowerCase().includes(nome.toLowerCase())) {
+        results.push(user)
+        return
+      }
+    }))
+    console.log({results})
+    setPersons(results)
   }
 
   async function Editar() {
     event.preventDefault();
-    
+
     const resultAPI = await api.get("/");
 
     const user = resultAPI.data.results[0];
-  
+
     Update(user);
     Load();
-    }
-  
-
-
+  }
 
   return (
     <div className={style.container}>
 
-    <CardPerson
-    cover="https://images.unsplash.com/photo-1536227661368-deef57acf708?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
-    avatar="https://github.com/WilliamSchreiner.png"
-    name="William Schreiner"
-    office= 'Jovem Aprendiz desenvovedor'/>
+      <CardPerson
+        cover="https://images.unsplash.com/photo-1536227661368-deef57acf708?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
+        avatar="https://github.com/WilliamSchreiner.png"
+        name="William Schreiner"
+        office='Jovem Aprendiz desenvovedor' />
 
-<div className={style.contatos}>
-<div  className={style.controles}>
-  <form >
-      
+      <div className={style.contatos}>
+        <div className={style.controles}>
+          <form >
+
             <div className={style.headerContatos}>
-            <h1 className={style.title}>Meus contatos</h1>
-              <div  className={style.buttons}>
-              <button  className={style.buttom} onClick={Adicionar}> <IoAdd /> </button>
-              <button  className={style.buttom} onClick={Editar}> <IoPencil /> </button>
-              <button  className={style.buttom} onClick={Deletar}> <BsFillTrash3Fill /> </button>
-              
+              <h1 className={style.title}>Meus contatos</h1>
+              <div className={style.buttons}>
+                <button className={style.buttom} onClick={Adicionar}> <IoAdd /> </button>
+                <button className={style.buttom} onClick={Editar}> <IoPencil /> </button>
+                <button className={style.buttom} onClick={Deletar}> <BsFillTrash3Fill /> </button>
+
+              </div>
             </div>
-            </div>
-            
+
             <div className={style.pesquisa}>
-            <button  className={style.buttomPesquisa}> <IoSearchOutline /> </button>
-            
-            <input type='text' name='pesquisa' className={style.inputPesquisa} placeholder="Busque por nome ou pelos dados do contato... "/>
+              <button className={style.buttomPesquisa} onClick={Pesquisar}> <IoSearchOutline /> </button>
+
+              <input type='text' name='pesquisa' className={style.inputPesquisa} placeholder="Busque por nome ou pelos dados do contato... " value={nome} onChange={() => { setNome(event.target.value) }} />
             </div>
-            
-            </form>
+
+          </form>
         </div>
 
-<div className={style.listaContatos}>
+        <div className={style.listaContatos}>
 
-<div className={style.listaCatalogo}>
-  <h1 className={style.letraCatalogo}>All</h1>
-  <div className={style.contatoCatalogo}>
+          <div className={style.listaCatalogo}>
+            <h1 className={style.letraCatalogo}>All</h1>
+            <div className={style.contatoCatalogo}>
 
-  {
-              persons.map((person, index) => (
-                <CardContato key= {index} 
-                avatar= {person.avatar}
-                name= {person.name}
-                celular= {person.celular}
+              {
+                persons.map((person, index) => (
+                  <CardContato key={index}
+                    avatar={person.avatar}
+                    name={person.name}
+                    celular={person.celular}
 
-                id= {person.id}
-                />
+                    id={person.id}
+                  />
 
-              ))
-            }
+                ))
+              }
 
-  </div> 
-</div>
-    
+            </div>
+          </div>
 
+
+        </div>
+      </div>
     </div>
-    </div>
-</div>    
   );
 }
