5ee6182269a8f4a5d9ba00c4ae4e82e52992ae60
William <williag.schreiner@gmail.com>
2023-05-29 22:28:58 -0300

    create tarefa29/05

--- PROG_MOBIII/mobile01/App.js
+++ PROG_MOBIII/mobile01/App.js
@@ -1,11 +1,26 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useEffect, useState} from 'react';
+import { StyleSheet, Text, View, TouchableOpacity, Alert, TextInput } from 'react-native';
 
 export default function App() {
+  const [text, setText] = useState("")
+
+  useEffect(() => {setText("Recado")}, []
+  )
+
+  function HandleButtom (){
+   setText("Recado Suspeito");
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Atras de Voce</Text>
+      <TouchableOpacity style={styles.buttom} onPress={HandleButtom}>
+        <Text style={{fontSize:16,  alignSelf: 'center'}}>Buttom</Text>
+      </TouchableOpacity>
+      <Text>{text}</Text>
       <StatusBar style="auto" />
+      <TextInput></TextInput>
     </View>
   );
 }
@@ -13,8 +28,20 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#00000',
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+
+  buttom:{
+    width:100, 
+    height:30, 
+    backgroundColor: "#f3f3f2",
+    marginTop: 35,
+    textAlign: 'center',
+    borderRadius: 10,
+
+
+  }
 });
